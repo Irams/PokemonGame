89a60fe88e4056816abeea8128c6cef2631aa33c
Irams <herber.medina@gmail.com>
2022-02-17 21:30:44 -0600

    App con componentes, el componente de imagen casi terminado

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>¿Quién es éste Pokémon?</h1>
+  <pokemon-picture :pokemonId="4" :showPokemon="true"/>
+  <pokemon-options/>
+</template>
+
+<script>
+
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+  components:{
+    PokemonPicture,
+    PokemonOptions
+  }
+}
+</script>
